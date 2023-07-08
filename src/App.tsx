c29a87369f01601ea73cd3bcb60166a56e9ac2d8
Dmitriy Kharchenko <dmitriy.kharchenko@sintez.co.za>
2023-07-08 15:24:15 +0300

    Added class component to see difference between functional component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from "react";
+import Counter from "./component/Counter";
+import ClassCounter from "./component/ClassCounter";
 
 function App() {
-  const [likes, setLikes] = React.useState<number>(0);
   const [value, setValue] = React.useState<string>("INPUT TEXT");
 
-  function increment(): void {
-    setLikes(likes + 1);
-  }
-
-  function decrement(): void {
-    setLikes(likes - 1);
-  }
   return (
     <div className="App">
-      <h1>{likes}</h1>
       <h1>{value}</h1>
       <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter />
+      <ClassCounter />
     </div>
   );
 }
