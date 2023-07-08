c9e836e642b820cce329467d64fcb226acdce4fb
Dmitriy Kharchenko <dmitriy.kharchenko@sintez.co.za>
2023-07-08 11:56:46 +0300

    Added simple counter & manageable element.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 function App() {
-  let likes = 0;
-  return <div className="App">
-    <button>Increment</button>
-    <button>Decrement</button>
-  </div>;
+  const [likes, setLikes] = React.useState<number>(0);
+  const [value, setValue] = React.useState<string>("INPUT TEXT");
+
+  function increment(): void {
+    setLikes(likes + 1);
+  }
+
+  function decrement(): void {
+    setLikes(likes - 1);
+  }
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;
